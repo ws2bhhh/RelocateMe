117b9c209f8268c2b3a03ecb55425ca95b601150
ws2bhhh <1160975470@qq.com>
2025-02-15 01:43:18 +0800

    Update Validate.swift

--- RelocateMe/Validate/Validate.swift
+++ RelocateMe/Validate/Validate.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 class Validate {
-    static func verify(completion: @escaping (Bool, Error?) -> Void) {
+    func verify(completion: @escaping (Bool, Error?) -> Void) {
         DispatchQueue.global().asyncAfter(deadline: .now() + 0.5) {
             completion(true, nil)
         }
